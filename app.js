a28a7c4f25057ef2f1c28027892a90c57da638d2
hodge-py <105604814+hodge-py@users.noreply.github.com>
2023-12-11 13:46:40 -0700

    done

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 var ip = require("ip");
 var path = require('path');
+var bodyParser = require('body-parser')
+const multer  = require('multer')
+var multiparty = require('multiparty');
 
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.json())
 
 port = process.env.PORT || 8080
 
@@ -16,4 +22,12 @@ app.use('/', express.static(__dirname + "/public"));
 serverMain = server.listen(port, () => {
   console.log(port);
 });
+
+
+app.post("/file-submit", (req, res) => {
+
+  console.log(req.file)
+  //res.sendFile(path.join(__dirname,'public/uploads/Cat03.jpg'))
+  res.send("complete");
+})
 
--- public/chatScr.js
+++ public/chatScr.js
@@ -13,6 +13,30 @@ $(document).ready(function () {
         $("#upload-file").css("display",'block');
      
     });
+
+    $(document).on('click',"#file-submit", async function () {
+
+        var myFile = $('#formFile').prop('files');
+        let formData = new FormData();
+        console.log(myFile[0].type)
+        var blob = new Blob([myFile[0]],{type: myFile[0].type})
+        blob = await blob.arrayBuffer()
+        //console.log(blob)
+        formData.append(myFile[0].name,blob)
+        $.ajax({
+            type: "POST",
+            url: "/file-submit",
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function(data){
+              console.log(data)
+            }
+          });
+
+
+    });
+
     
 
 
