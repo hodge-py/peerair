2160d9353d4747890fde4773a3c5fb16a4ea2ff5
hodge-py <105604814+hodge-py@users.noreply.github.com>
2023-12-11 11:13:46 -0700

    changes

--- public/chatScr.js
+++ public/chatScr.js
@@ -1,10 +1,18 @@
 $(document).ready(function () {
     
 
+    $(document).on('click',"#file", function () {
+        $("#upload-file").css("display",'none');
+        $("#file-table").css("display",'table');
+     
+    });
 
 
-
-
+    $(document).on('click',"#upload", function () {
+        $("#file-table").css("display",'none');
+        $("#upload-file").css("display",'block');
+     
+    });
     
 
 
